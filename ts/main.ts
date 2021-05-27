cae9788a5ea4769547d1faf97c90128fa9cf0084
Mike <63655075+MixelTe@users.noreply.github.com>
2021-05-27 19:15:14 +0300

    init

--- ts/main.ts
+++ ts/main.ts
@@ -9,38 +9,27 @@ main();
 
 async function main()
 {
-	console.log("Start main func");
 	tge.clear();
 	tge.print("Hello, it's a cool game!");
 	tge.print("Enter character name:");
 	const name = await tge.text(2);
-	// const name = "Harry";
 	tge.print(`${name} enters the dungeon.`);
 	await tge.wait(1);
 	tge.print(`${name} noticed something!`);
 	await tge.wait(1);
 	tge.print("It's a door!");
 	tge.print("There is a question on the door:");
-	if (!false)
+	let answer = 0;
+	let firstMistake = true;
+	tge.print("What is 2+2?", true);
+	answer = await tge.num();
+	while (answer != 4)
 	{
-		let answer = 0;
-		let firstMistake = true;
-		tge.print("What is 2+2?", true);
+		tge.clear(firstMistake ? 2 : 3);
+		firstMistake = false;
+		tge.print('You hear: "&0Wrong answer!&c". Try again.', true);
+		tge.print("What is 2+2?");
 		answer = await tge.num();
-		while (answer != 4)
-		{
-			tge.clear(firstMistake ? 2 : 3);
-			firstMistake = false;
-			tge.print('You hear: "&0Wrong answer!&c". Try again.', true);
-			tge.print("What is 2+2?");
-			answer = await tge.num();
-		}
-	}
-	else
-	{
-		tge.print("What is 2+2?", true);
-		const answer = await tge.num(2, 20);
-		if (answer != 4) tge.print(`You hear: "Wrong answer! But I'm kind today"`);
 	}
 	tge.print(`The door opened and ${name} entered the room.`);
 	await tge.wait();
@@ -62,7 +51,6 @@ async function main()
 	tge.clear();
 
 	tge.print("You are at home.");
-	tge.print(`${name} is hungry.`);
 	tge.print(`${name} goes to the kitchen.`);
 	await tge.wait(2);
 	tge.print(`${name} poured tea, and puts sugar.`);
@@ -82,5 +70,4 @@ async function main()
 	tge.print();
 	await tge.choose(["Play again"]);
 	setTimeout(main, 100);
-	console.log("End main func");
 }
