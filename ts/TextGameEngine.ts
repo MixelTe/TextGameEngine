fcc1eff2d672191dc0161ab799603edef5d54d2d
Mike <63655075+MixelTe@users.noreply.github.com>
2021-05-27 21:56:39 +0300

    v1.2

--- ts/TextGameEngine.ts
+++ ts/TextGameEngine.ts
@@ -253,7 +253,7 @@ export class Titles
 	/**Text "Tap to continue" when called TextGameEngine.wait with -1*/
 	public tapToCon = "Tap here to continue";
 	/**Engine version. Assign an empty string to remove version tag.*/
-	public version = "Version: 1.1"; //1.1
+	public version = "Version: 1.2"; //1.2
 }
 
 class Line
